67644d08727bc0ab907d161a7fa0a19a97d80e86
Stefan Stefu <stefu1234@gmail.com>
2019-04-20 11:41:52 +0300

    ceva

--- src/main/java/com/sda/springexample/root/Main.java
+++ src/main/java/com/sda/springexample/root/Main.java
@@ -1,11 +1,16 @@
 package com.sda.springexample.root;
-
+import com.sda.springexample.production.Worker;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        String[] beans = new String[]{"tools.xml", "worker.xmml"};
+        String[] beans = new String[]{"tools.xml", "worker.xml"};
         ApplicationContext xmlApplicationContext = new ClassPathXmlApplicationContext(beans);
+
+        Worker testWorker =  xmlApplicationContext.getBean(Worker.class);
+        System.out.println(testWorker.getAge());
+        System.out.println(testWorker.getProffesion());
+
     }
 }
