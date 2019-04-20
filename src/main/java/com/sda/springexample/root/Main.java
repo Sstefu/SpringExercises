e98a15ce93a2164ae73bfffaaaaf05cba5df72aa
Stefan Stefu <stefu1234@gmail.com>
2019-04-20 11:55:36 +0300

    something

--- src/main/java/com/sda/springexample/root/Main.java
+++ src/main/java/com/sda/springexample/root/Main.java
@@ -1,4 +1,5 @@
 package com.sda.springexample.root;
+import com.sda.springexample.production.Tool;
 import com.sda.springexample.production.Worker;
 import com.sda.springexample.production.WorkerMotto;
 import org.springframework.context.ApplicationContext;
@@ -8,10 +9,13 @@ public class Main {
     public static void main(String[] args) {
         String[] beans = new String[]{"tools.xml", "worker.xml"};
         ApplicationContext xmlApplicationContext = new ClassPathXmlApplicationContext(beans);
+        Tool testTool = xmlApplicationContext.getBean(Tool.class);
 
         Worker testWorker =  xmlApplicationContext.getBean(Worker.class);
         System.out.println(testWorker.getAge());
         System.out.println(testWorker.getProffesion());
+        System.out.println(testTool.getWeight() + " " + testTool.getName() );
+        System.out.println();
 
     }
 }
